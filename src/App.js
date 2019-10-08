3e7e69f5f45519a073afefde7a1042417bf7cf7f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-10-08 19:27:41 +0300

    added propTypes to App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import pt from 'prop-types'
+class App extends React.Component {
+    
+    static propTypes = {
+        store: pt.object.isRequired
+    }
+
+    render() {
+        return (<span>PLACEHOLDER</span>)
+    }
+}
+export default App
---
+++ src/List.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class List extends React.Component {
+    render() {
+        return (<span>PLACEHOLDER</span>)
+    }
+}
+export default List
