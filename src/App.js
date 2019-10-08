f57b1e1c1cdc60a31cd17e43f9d8ffb6cb302094
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-10-08 19:47:51 +0300

    halfway

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import pt from 'prop-types'
+import List from './List'
 class App extends React.Component {
-    
+
     static propTypes = {
         store: pt.object.isRequired
     }
 
     render() {
-        return (<span>PLACEHOLDER</span>)
+        // const store =  this.props.store
+        const { store } = this.props
+
+        const listsProps = store.lists.map(listInfo => {
+            return {
+                header: listInfo.header,
+                cards: listInfo.cardIds.map(id => store.allCards[id])
+            }
+        })
+        console.log(`listsprops:`, listsProps)
+
+        const lists = listsProps.map((p,index) => <List key={index} {...p} />)
+
+        return (
+            <main class="App">
+                <header class="App-header">
+                    <h1>Trelloyes!</h1>
+                </header>
+                <div class="App-list">
+                    {lists}
+                </div>
+            </main>
+        )
     }
 }
 export default App
--- src/List.js
+++ src/List.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import pt from 'prop-types'
 class List extends React.Component {
+    static propTypes = {
+        header: pt.string.isRequired,
+        cards: pt.array.isRequired,
+    }
     render() {
-        return (<span>PLACEHOLDER</span>)
+        return (
+            <section class="List">
+                        <header class="List-header">
+                            <h2>{this.props.header}</h2>
+                        </header>
+                        <div class="List-cards">
+                            <div class="Card">
+                                <button type="button">delete</button>
+                                <h3>First card</h3>
+                                <p>lorem ipsum</p>
+                            </div>
+                            <div class="Card">
+                                <button type="button">delete</button>
+                                <h3>TODO replace me with a card component!!</h3>
+                                <p>lorem ipsum</p>
+                            </div>
+                           
+                            <button type="button" class="List-add-button">
+                                + Add Random Card
+                    </button>
+                        </div>
+                    </section>
+        )
     }
 }
 export default List
